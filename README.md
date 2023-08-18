cb3ca35e9596505b665ffd7b8f944c04360b5e12
lulamae12 <tls.tommy11@gmail.com>
2023-08-18 16:06:25 -0400

    finished pdf merger

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# multipdf
-combines multiple PDF files into one large PDF
+# pdf-merger
+pdf-merger is a simple pdf merging program. pdfs put in the Input Directory will be merged into one singular PDF file, 'Merged.pdf' in the output directory
--- pdf-merger.py
+++ pdf-merger.py
@@ -35,21 +35,65 @@ class main:
         for file in os.listdir(self.inputDir):
             fileStats = os.stat(os.path.join(self.inputDir, file))
             fileData = [file, str(datetime.datetime.utcfromtimestamp(fileStats.st_mtime)), str(f"{fileStats.st_size/(1<<10):,.0f} KB")]
-            self.pdfFiles.append(file)
+            self.pdfFiles.append(os.path.join(self.inputDir,file))
             tableData.append(fileData)
         print("\nThe following PDF files were found in the input directory:\n")
         headers = ["File", "Last Modified", "Size"]
         print(tabulate(tableData, headers=headers, tablefmt="fancy_grid"))
-    
-    def sort(self,method):
+       
+    def sort(self):
         while True:
-            print("In which order would you like the files to be mergerd? ")
-            print("[1] Alphabetically By Filename")
-            print("[2] By Date")
+            print("\nIn which order would you like the files to be mergerd? ")
+            print("[1] Alphabetically By Filename [A-Z]")
+            print("[2] Alphabetically By Filename [Z-A]")
+            print("[3] By Date")
             userInput = input("Method: ")
-            if userInput
+            if userInput == "1":
+                self.pdfFiles.sort()
+                print("Sorted files alphabetically!\n")
+                break
+            elif userInput == "2":
+                self.pdfFiles.sort(key=lambda x: os.path.getmtime(x))
+                self.pdfFiles.reverse()
+                print("Sorted files alphabetically!\n")
+                break
+            elif userInput == "3":
+                self.pdfFiles.sort(key=lambda x: os.path.getmtime(x))
+                print("Sorted files by date!\n")
+                break    
+
+            else:
+                print("\n[Error] Invalid Input please choose a correct option. (1 - 3)")
+
+    def buildMergedPdf(self):
+        
+        while True:
+            print("\nAll files in the input directory will be merged into 'Merged.pdf' in the output directory.")
+            userInput = input("Would you like to proceed (Y/n)? ")
+            if(userInput.upper() == "Y" or userInput.upper() == "YES"):
+                print("\nMerging...\n")
+                break
+            elif(userInput.upper() == "N" or userInput.upper() == "NO"):
+                print("\nMerge canceled!\n")
+                return
+            else:
+                print("\n[Error] Invalid Input!\n")
+                continue
 
 
+        masterPDFPages = []
+        writer = PyPDF2.PdfWriter()
+
+        for pdf in self.pdfFiles:
+            masterPDFPages.append(PyPDF2.PdfReader(pdf))
+
+        for pdf in masterPDFPages:
+            for page in range(len(pdf.pages)):
+                writer.add_page(pdf.pages[page])
+        writer.write(os.path.join(self.outputDir,"Merged.pdf"))
+
+
+        print("\n[Success] Wrote merged pdf to: " + str(self.outputDir) + "\Merged.pdf")
 
 
     #main
@@ -61,5 +105,6 @@ class main:
         
         self.listFiles()
         self.sort()
+        self.buildMergedPdf()
 
 merger = main()

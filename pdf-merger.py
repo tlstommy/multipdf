f6b11bfdbc35bccf8b324ec62b1ed83fd9da4cbe
lulamae12 <tls.tommy11@gmail.com>
2023-08-18 14:37:18 -0400

    commit todo sort

--- pdf-merger.py
+++ pdf-merger.py
@@ -1,4 +1,5 @@
-import PyPDF2, os
+import PyPDF2, os, stat, datetime
+from tabulate import tabulate
 
 INPUT_DIR = "input"
 OUTPUT_DIR = "output"
@@ -8,8 +9,11 @@ class main:
         self.cwd = os.getcwd()
         self.inputDir = os.path.join(self.cwd,INPUT_DIR)
         self.outputDir = os.path.join(self.cwd,OUTPUT_DIR)
+        self.pdfFiles = []
+
         self.dirExistsCheck()
         self.pdfMerger()
+        
       
     #check if input and output dirs exist
     def dirExistsCheck(self):
@@ -26,7 +30,27 @@ class main:
 
     #list all the input files and their info
     def listFiles(self):
-        
+
+        tableData = []
+        for file in os.listdir(self.inputDir):
+            fileStats = os.stat(os.path.join(self.inputDir, file))
+            fileData = [file, str(datetime.datetime.utcfromtimestamp(fileStats.st_mtime)), str(f"{fileStats.st_size/(1<<10):,.0f} KB")]
+            self.pdfFiles.append(file)
+            tableData.append(fileData)
+        print("\nThe following PDF files were found in the input directory:\n")
+        headers = ["File", "Last Modified", "Size"]
+        print(tabulate(tableData, headers=headers, tablefmt="fancy_grid"))
+    
+    def sort(self,method):
+        while True:
+            print("In which order would you like the files to be mergerd? ")
+            print("[1] Alphabetically By Filename")
+            print("[2] By Date")
+            userInput = input("Method: ")
+            if userInput
+
+
+
 
     #main
     def pdfMerger(self):
@@ -36,5 +60,6 @@ class main:
             input("[WARN] The input is directory is empty, please place the pdfs to merge inside and press enter to continue\n")
         
         self.listFiles()
+        self.sort()
 
 merger = main()
